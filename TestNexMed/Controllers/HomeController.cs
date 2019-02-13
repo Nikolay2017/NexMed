6e4d76818e2ef7bb16e55fd6088ba14a78fc4ba9
Nikolay2018 <pisarev2@rambler.ru>
2019-02-13 16:55:40 +0300

    Add index

--- TestNexMed/Controllers/HomeController.cs
+++ TestNexMed/Controllers/HomeController.cs
@@ -1,50 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Newtonsoft.Json.Linq;
 using TestNexMed.Models;
 
 namespace TestNexMed.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public HomeController()
         {
+        }
+
+        public HomeController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            ViewBag.Message = "";
+            var userId = User.Identity.GetUserId();
+            var currentUser = await UserManager.FindByIdAsync(userId);
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            response = await client.GetAsync(
+                $"https://api.weatherbit.io/v2.0/current?city={currentUser.Sity}&key=58c2500edd1b4308aa4bf5063a7fcb03");
+            if (response.IsSuccessStatusCode)
+            {
+                HttpContent content = response.Content;
+                string result = await content.ReadAsStringAsync();
+                ModelWeather.RootObject dataRootObject = JToken.Parse(result).ToObject<ModelWeather.RootObject>(); //json2charp.com
+                ViewBag.Message =
+                    $"Температура твоего города({currentUser.Sity}) = {dataRootObject.data.First().temp.ToString(CultureInfo.InvariantCulture)}";
+            }
+            else
+            {
+                
+            }
+
+            
+           
+
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "на "+DateTime.Now.Date.ToShortDateString();
-            Task t = new Task(RequestWether);
-            t.Start();
-            
+            //Task t = new Task(RequestWether);
+            //t.Start();
             return View();
-        }
-
-        private async void RequestWether()
-        {
-            using (HttpClient client = new HttpClient())
-            using (HttpResponseMessage response = await client.GetAsync("https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=58c2500edd1b4308aa4bf5063a7fcb03"))
-            using (HttpContent content = response.Content)
-            {
-                if (response.IsSuccessStatusCode)
-                {
-                    string result = await content.ReadAsStringAsync();
-
-                }
-            }
         }
 
         public ActionResult Arxive()
         {
             ViewBag.Message = "-----------------------";
-
             return View();
         }
     }
---
+++ TestNexMed/Models/ModelWeather.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestNexMed.Models
+{
+    public class ModelWeather
+    {
+        public class Weather
+        {
+            public string icon { get; set; }
+            public string code { get; set; }
+            public string description { get; set; }
+        }
+
+        public class Datum
+        {
+            public string wind_cdir { get; set; }
+            public int rh { get; set; }
+            public string pod { get; set; }
+            public double pres { get; set; }
+            public string timezone { get; set; }
+            public Weather weather { get; set; }
+            public string country_code { get; set; }
+            public int clouds { get; set; }
+            public int ts { get; set; }
+            public int solar_rad { get; set; }
+            public string state_code { get; set; }
+            public double wind_spd { get; set; }
+            public double lat { get; set; }
+            public string wind_cdir_full { get; set; }
+            public int slp { get; set; }
+            public int vis { get; set; }
+            public double lon { get; set; }
+            public string sunrise { get; set; }
+            public string datetime { get; set; }
+            public int h_angle { get; set; }
+            public double dewpt { get; set; }
+            public int snow { get; set; }
+            public int uv { get; set; }
+            public int wind_dir { get; set; }
+            public double elev_angle { get; set; }
+            public int ghi { get; set; }
+            public int dhi { get; set; }
+            public int dni { get; set; }
+            public string city_name { get; set; }
+            public int precip { get; set; }
+            public string sunset { get; set; }
+            public double temp { get; set; }
+            public string station { get; set; }
+            public string ob_time { get; set; }
+            public double app_temp { get; set; }
+        }
+
+        public class RootObject
+        {
+            public List<Datum> data { get; set; }
+            public int count { get; set; }
+        }
+    }
+}
