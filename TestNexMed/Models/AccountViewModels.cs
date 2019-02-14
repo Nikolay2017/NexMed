69c9e914134c264758722488a3a960278ed111e5
Nikolay2018 <pisarev2@rambler.ru>
2019-02-14 09:43:32 +0300

    edit autentification

--- TestNexMed/Models/AccountViewModels.cs
+++ TestNexMed/Models/AccountViewModels.cs
@@ -49,9 +49,12 @@ namespace TestNexMed.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Адрес электронной почты")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Имя")]
+        public string UserName { get; set; }
+        //[Required]
+        //[Display(Name = "Адрес электронной почты")]
+        //[EmailAddress]
+        //public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
